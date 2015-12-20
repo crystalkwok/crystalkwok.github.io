68bca6e48331a20b26e634bebcd57779f6bb52b3
Crystal Kwok <xcrystal.kwokx@gmail.com>
2015-12-20 01:24:35 -0800

    extreme overhaul of portfolio lol

--- navbar.js
+++ navbar.js
@@ -1,35 +1,27 @@
 $(document).ready(function(){
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 115) {
-    $('.navbar').addClass('shrink').removeClass('bigger');
-  } else {
-    $('.navbar').removeClass('shrink').addClass('bigger');
-  }
-});
-    
-
-
-$('.icon-menu').click(function(){
-					$('.dropdown').toggle();
-    console.log('hi');
-					
-				});
-                
-
-
-				$('.close').click(function(){
-					$('.dropdown').hide();
-				});
-                
-          
-  $('.fa_ux_dropdown').click(function() {
-    $('.dropdown-menu').toggle();
+  //make navbar smaller when shrink
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 110) {
+        $('.navbar').addClass('shrink').removeClass('bigger');
+      } else {
+        $('.navbar').removeClass('shrink').addClass('bigger');
+      }
+    });
+                  
+  //dropdown menu for ux toggles based on angle down        
+    $('.ux_dropdown_big').click(function() {
+      $('.dropdown-menu').toggle();
       return false;
-  });
-                
-                 $('.fa_ux_dropdown_small').click(function() {
-    $('.dropdown_ux').toggle();
-  });
+    });
 
-			});
+    //dropdown menu for ux (xs screen)
+    $('#fa_ux').click(function(){
+      $('.ux_dropdown_small').toggleClass('show');
+    });
+    $('#close_ux').click(function(){
+
+      $('.ux_dropdown_small').removeClass('show');
+    });
+
+});
