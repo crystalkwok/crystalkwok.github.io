2645fceb22a0d524c580f69307f87733dceed0dd
Crystal Kwok <xcrystal.kwokx@gmail.com>
2016-06-18 15:41:30 -0700

    finish design lab
    add scroll animation

--- navbar.js
+++ navbar.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
 
+var divs = $('.body .overlay'),
+    limit = 230; 
+
+
   //make navbar smaller when shrink
     $(window).scroll(function() {
-      if ($(document).scrollTop() > 110) {
+      if ($(document).scrollTop() > 120) {
         $('.navbar').addClass('shrink').removeClass('bigger');
       } else {
         $('.navbar').removeClass('shrink').addClass('bigger');
       }
+
+        var st = $(this).scrollTop();
+
+         /* avoid unnecessary call to jQuery function */
+         if (st <= limit) {
+            divs.css({ 'opacity' : (st/limit) });
+        }
+
+
     });
                   
   //dropdown menu for ux toggles based on angle down        
