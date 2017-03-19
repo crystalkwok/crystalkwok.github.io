85aa8520acce298550877e3e087b6a5cf42ad069
Crystal Kwok <xcrystal.kwokx@gmail.com>
2017-03-18 22:36:21 -0700

    finish design lab
    finish responsiveness and add in new bootstrap and jQuery

--- navbar.js
+++ navbar.js
@@ -47,4 +47,24 @@ var divs = $('.body .overlay'),
       $('.ux_dropdown_small').removeClass('show');
     });
 
+    $(".side-nav ul li a[href^='#']").on('click', function(e) {
+
+       // prevent default anchor click behavior
+       e.preventDefault();
+
+       // store hash
+       var hash = this.hash;
+
+       // animate
+       $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 300, function(){
+
+           // when done, add hash to url
+           // (default click behaviour)
+           window.location.hash = hash;
+    });
+
+});
+
 });
---
+++ side-nav.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+		$(".fancybox").fancybox();
+
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+
+	});       
