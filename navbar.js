54d8a62c5a2d09a5460572223aa21388144e7a3c
Crystal Kwok <xcrystal.kwokx@gmail.com>
2022-02-23 00:26:37 -0800

    v3 Bluebeam Studio Changes

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 var divs = $('.body .overlay'),
-    limit = 230; 
+    limit = 230;
 
 
   //make navbar smaller when shrink
@@ -11,7 +11,7 @@ var divs = $('.body .overlay'),
       } else {
         $('.navbar').removeClass('shrink').addClass('bigger');
       }
-        
+
         var target = $(".section:first-of-type").offset().top;
     if ($(document).scrollTop() >= target) {
         $('.side-nav').addClass('fix');
@@ -19,7 +19,7 @@ var divs = $('.body .overlay'),
         else {
         $('.side-nav').removeClass('fix');
       }
-        
+
 
 
         var st = $(this).scrollTop();
@@ -31,34 +31,19 @@ var divs = $('.body .overlay'),
 
 
     });
-                  
-  //dropdown menu for ux toggles based on angle down        
-    $('.ux_dropdown_big').click(function() {
-      $('.dropdown-menu.all-projects').toggle();
-      return false;
-    });
 
-      //dropdown menu for viewing more projects     
+      //dropdown menu for viewing more projects
     $('.view-more-proj').click(function() {
       $('.more-projects.dropdown-menu').toggle();
       $('.view-more-proj .description').toggleClass("semibold");
       return false;
     });
 
-    //dropdown menu for ux (xs screen)
-    $('#fa_ux').click(function(){
-      $('.ux_dropdown_small').toggleClass('show');
-    });
-    $('#close_ux').click(function(){
 
-      $('.ux_dropdown_small').removeClass('show');
-    });
 
     $(".side-nav ul li a[href^='#']").on('click', function(e) {
-
        // prevent default anchor click behavior
        e.preventDefault();
-
        // store hash
        var hash = this.hash;
 
@@ -66,11 +51,12 @@ var divs = $('.body .overlay'),
        $('html, body').animate({
            scrollTop: $(hash).offset().top
          }, 300, function(){
-
            // when done, add hash to url
            // (default click behaviour)
            window.location.hash = hash;
     });
+
+
 
 });
 
